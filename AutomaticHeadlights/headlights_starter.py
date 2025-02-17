548f822675646d72cabccb5217fcbe99482e2d0a
Drew Ray <123132668+drewray80@users.noreply.github.com>
2025-02-17 15:09:07 -0500

    Add files via upload

---
+++ AutomaticHeadlights/headlights_starter.py
@@ -0,0 +1,20 @@
+# Imports go at the top
+from microbit import *
+import sphero
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    if display.read_light_level() < 20:
+        sphero.RVRLed.set_rgb_led_by_index(sphero.LEDs.RIGHT_HEADLIGHT,255,255,255)
+        sphero.RVRLed.set_rgb_led_by_index(sphero.LEDs.LEFT_HEADLIGHT,255,255,255)
+        sphero.RVRLed.set_rgb_led_by_index(sphero.LEDs.RIGHT_BRAKELIGHT,255,0,0)
+        sphero.RVRLed.set_rgb_led_by_index(sphero.LEDs.LEFT_BRAKELIGHT,255,0,0)
+        sphero.RVRLed.set_rgb_led_by_index(sphero.LEDs.LEFT_STATUS,134,1,175)
+        sphero.RVRLed.set_rgb_led_by_index(sphero.LEDs.RIGHT_STATUS,134,1,175)
+    else:
+        sphero.RVRLed.set_rgb_led_by_index(sphero.LEDs.RIGHT_HEADLIGHT,0,0,0)
+        sphero.RVRLed.set_rgb_led_by_index(sphero.LEDs.LEFT_HEADLIGHT,0,0,0)
+        sphero.RVRLed.set_rgb_led_by_index(sphero.LEDs.RIGHT_BRAKELIGHT,0,0,0)
+        sphero.RVRLed.set_rgb_led_by_index(sphero.LEDs.LEFT_BRAKELIGHT,0,0,0)
+        sphero.RVRLed.set_rgb_led_by_index(sphero.LEDs.LEFT_STATUS,0,0,0)
+        sphero.RVRLed.set_rgb_led_by_index(sphero.LEDs.RIGHT_STATUS,0,0,0)
